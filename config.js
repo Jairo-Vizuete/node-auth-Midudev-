0a1d0c6589844c9fef1436d95af6077301f15e0d
Jairo Vizuete <jpedraza@ccffaa.mil.ec>
2024-06-14 14:19:32 -0500

    End tuto Midudev

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const { PORT = 3000, SALT_ROUNDS = 10 } = process.env;
+export const {
+  PORT = 3000,
+  SALT_ROUNDS = 10,
+  SECRET_KEY = "this-is-an-awesome-secret-key", // This secret key must have hash and a lot of things!
+} = process.env;
--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, SECRET_KEY } from "./config.js";
 import { UserRepository } from "./user-repository.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  const token = req.cookies.access_token;
+  let data = null;
+
+  req.session = { user: null };
+
+  try {
+    data = jwt.verify(token, SECRET_KEY);
+    req.session.user = data;
+  } catch (error) {
+    req.session.user = null;
+  }
+
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.json({
-    sender: "Jairo",
-    message: "Hi bro!",
-  });
+  const { user } = req.session;
+  res.render("index", user);
 });
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await UserRepository.login({ username, password });
-    res.send({user});
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res
+      .cookie("access_token", token, {
+        httpOnly: true, // The cookie is only allowed on the server
+        secure: process.env.NODE_ENV === "production", // It's only allowed in https
+        sameSite: "strict", // It's only allowed in the same domain
+        maxAge: 1000 * 60 * 60, // Life cycle
+      })
+      .send({ user, token });
   } catch (error) {
     res.status(401).send(error.message);
   }
@@ -33,9 +65,17 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/logout", (req, res) => {});
+app.post("/logout", (req, res) => {
+  res.clearCookie("access_token").json({ message: "logout successful" }); // here we can redirect
+});
 
-app.get("/protected", (req, res) => {});
+app.get("/protected", (req, res) => {
+  const { user } = req.session;
+
+  if (!user) return res.status(401).send("access not authorized");
+
+  res.render("protected", user);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
